0debe16dc75f7086c6dfeb8dd20e68ddd8e54f3c
robhhr <robhhr@outlook.com>
2020-07-16 17:44:55 -0700

    fix component imports

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,12 +1,14 @@
 import {blue, neutral} from './colors'
 import {primaryFont} from './typography'
 
-export const defaultTheme = {
- primaryColor: blue(200),
- primaryColorHover: blue(400),
- primaryColorActive: blue(300),
- textColorOnPrimary: neutral(100),
- textColor: neutral(600),
- textColorInverted: neutral(100),
+const defaultTheme = {
+ primaryColor: blue[200],
+ primaryColorHover: blue[400],
+ primaryColorActive: blue[300],
+ textColorOnPrimary: neutral[100],
+ textColor: neutral[600],
+ textColorInverted: neutral[100],
  primaryFont
 }
+
+export {defaultTheme}
