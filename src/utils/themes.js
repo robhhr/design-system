23a3e9b5f1fd428940cc448969f045e7914ef584
robhhr <robhhr@outlook.com>
2020-07-17 00:51:35 -0700

    fix merge conflicts

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -2,17 +2,13 @@ import {blue, neutral} from './colors'
 import {primaryFont} from './typography'
 
 const defaultTheme = {
- primaryColor: blue[200],
- primaryColorHover: blue[400],
- primaryColorActive: blue[300],
- textColorOnPrimary: neutral[100],
- textColor: neutral[600],
- textColorInverted: neutral[100],
- primaryFont
+  primaryColor: blue[200],
+  primaryColorHover: blue[400],
+  primaryColorActive: blue[300],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  primaryFont,
 }
 
-<<<<<<< HEAD
 export {defaultTheme}
-=======
-export default defaultTheme
->>>>>>> a60bca706f07060436442da10214b804338bcbaf
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,6 @@
 const primaryFont = "'Cabin', monospace"
 
 const typeScale = {
-<<<<<<< HEAD
   header1: '1.8rem',
   header2: '1.6rem',
   header3: '1.4rem',
@@ -13,16 +12,3 @@ const typeScale = {
 }
 
 export {primaryFont, typeScale}
-=======
-  header1: "1.8rem",
-  header2: "1.6rem",
-  header3: "1.4rem",
-  header4: "1.2rem",
-  header5: "1.1rem",
-  paragraph: "1rem",
-  helperText: "0.8rem",
-  copyrightText: "0.7rem"
-}
-
-export {primaryFont, typeScale}
->>>>>>> a60bca706f07060436442da10214b804338bcbaf
