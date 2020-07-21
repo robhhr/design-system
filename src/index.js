d7cce4e3ccdbcd11ddeebfe499f57090e14e0fe3
robhhr <robhhr@outlook.com>
2020-07-20 19:19:11 -0700

    add theme provider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
+import {ThemeProvider} from 'styled-components'
 import {
   PrimaryButton,
   SecondaryButton,
   TertiaryButton,
 } from './components/Buttons'
-import {GlobalStyle} from './utils'
+import {GlobalStyle, darkTheme, defaultTheme} from './utils'
 
-const App = () => (
-  <>
-    <PrimaryButton modifiers={['small', 'success']}>Hello World</PrimaryButton>
-    <SecondaryButton modifiers={['lage', 'warning', 'warningSecondary']}>
-      Hello World
-    </SecondaryButton>
-    <TertiaryButton modifiers={['small', 'error']}>Hello World</TertiaryButton>
-    <GlobalStyle />
-  </>
-)
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false)
+  return (
+    <ThemeProvider theme={darkTheme ? darkTheme : defaultTheme}>
+      <PrimaryButton modifiers={['small', 'success']}>
+        Hello World
+      </PrimaryButton>
+      <SecondaryButton modifiers={['lage', 'warning', 'warningSecondary']}>
+        Hello World
+      </SecondaryButton>
+      <TertiaryButton modifiers={['small', 'error']}>
+        Hello World
+      </TertiaryButton>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'))
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -25,4 +25,26 @@ const defaultTheme = {
   },
 }
 
-export {defaultTheme}
+const darkTheme = {
+  primaryColor: neutral[100],
+  primaryColorHover: neutral[200],
+  primaryColorActive: neutral[100],
+  textOnDisabled: neutral[300],
+  textColorOnPrimary: blue[300],
+  textColor: blue[300],
+  textColorInverted: neutral[100],
+  primaryFont,
+  status: {
+    warningColor: yellow[100],
+    warningColorHover: yellow[200],
+    warningColorActive: yellow[300],
+    errorColor: red[100],
+    errorColorHover: red[200],
+    errorColorActive: red[300],
+    successColor: green[100],
+    successColorHover: green[200],
+    successColorActive: green[300],
+  },
+}
+
+export {defaultTheme, darkTheme}
