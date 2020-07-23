5d9e95b73070b02796f5968351bf9183968d7465
robhhr <robhhr@outlook.com>
2020-07-23 15:09:04 -0700

    create main buttons stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,16 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-docs',
+    '@storybook/addon-contexts/register',
+    '@storybook/addon-backgrounds/register'
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
   ],
 }
